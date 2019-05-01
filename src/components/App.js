d558328bb2896df19b5801f004d854604574fdda
George Tang <gtang43@gmail.com>
2019-04-30 21:29:05 -0700

    Used context instead of redux to share data across components

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,19 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../context/LanguageContext';
+import { LanguageStore } from '../context/LanguageContext';
 import ColorContext from '../context/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component {
-  state = { language: 'english' };
-
-  onLanguageChange = language => {
-    this.setState({ language });
-  };
-
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select language:
-          <i
-            className="flag us"
-            onClick={() => this.onLanguageChange('english')}
-          />
-          <i
-            className="flag nl"
-            onClick={() => this.onLanguageChange('dutch')}
-          />
-        </div>
-        <ColorContext.Provider value="red">
-          <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value="red">
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
